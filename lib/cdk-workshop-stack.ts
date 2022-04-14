9e8d0cc5d9bfea0c4fa2d0e1c1869fc6b6963460
Minh T Nguyen <minh.nguyen@service.vic.gov.au>
2022-04-14 11:30:56 +1000

    aws workshop

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -3,17 +3,26 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+    //   visibilityTimeout: Duration.seconds(300),
+    // });
+    // const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    // topic.addSubscription(new subs.SqsSubscription(queue));
+
+    // define an AWS Lmbda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler',
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new apigw.LambdaRestApi(this, 'EndPoint', { handler: hello });
   }
 }
