bfde26242d18bbc4af07a26e60997ecbe1f6e19a
Minh T Nguyen <minh.nguyen@service.vic.gov.au>
2022-04-14 14:18:40 +1000

    test lambda function

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -17,3 +17,33 @@ test('DynamoDB table created', () => {
   const template = Template.fromStack(stack);
   template.resourceCountIs('AWS::DynamoDB::Table', 1);
 });
+
+test('Lambda has environment variables', () => {
+  const stack = new cdk.Stack();
+
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda'),
+    }),
+  });
+
+  const template = Template.fromStack(stack);
+
+  const envCapture = new Capture();
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Environment: envCapture,
+  });
+
+  expect(envCapture.asObject()).toEqual({
+    Variables: {
+      DOWNSTREAM_FUNCTION_NAME: {
+        Ref: 'TestFunction22AD90FC',
+      },
+      HITS_TABLE_NAME: {
+        Ref: 'MyTestConstructHits24A357F0',
+      },
+    },
+  });
+});
